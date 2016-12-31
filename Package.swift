7e7becad0c2a3e7ddc5cf11aa5bf7ed2561e9827
mominul <mominul2082@gmail.com>
2016-12-31 16:16:41 +0600

    Get rid of CEnvironment

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
   name: "swiftup",
   dependencies: [
   .Package(url: "https://github.com/kylef/Commander.git", Version(0, 6, 0)),
-  .Package(url: "https://github.com/Danappelxx/Environment.git", Version(0, 6, 0)),
+  .Package(url: "https://github.com/mominul/Environment.git", Version(0, 7, 0)),
   ]
 )
