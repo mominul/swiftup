20d6b33f4e89c6f72932b050f2696c3fcb19a212
mominul <mominul2082@gmail.com>
2017-01-20 16:24:32 +0600

    Fix install.sh

--- install.sh
+++ install.sh
@@ -3,15 +3,15 @@
 # swiftup installer
 
 setupShell() {
-  if [ -f "${HOME}/.bash_profile" ]; then
+  if [ -f "$HOME/.bash_profile" ]; then
     echo 'export SWIFTUP_ROOT="$HOME/.swiftup"' >> ~/.bash_profile
     echo 'export PATH="$SWIFTUP_ROOT/bin:$PATH"' >> ~/.bash_profile
     echo 'export PATH="$SWIFTUP_ROOT/shims:$PATH"' >> ~/.bash_profile
-  elif [ -f "${HOME}/.bashrc" ]; then
+  elif [ -f "$HOME/.bashrc" ]; then
     echo 'export SWIFTUP_ROOT="$HOME/.swiftup"' >> ~/.bashrc
     echo 'export PATH="$SWIFTUP_ROOT/bin:$PATH"' >> ~/.bashrc
     echo 'export PATH="$SWIFTUP_ROOT/shims:$PATH"' >> ~/.bashrc
-  elif [ -f "${HOME}/.zshrc" ]; then
+  elif [ -f "$HOME/.zshrc" ]; then
     echo 'export SWIFTUP_ROOT="$HOME/.swiftup"' >> ~/.zshrc
     echo 'export PATH="$SWIFTUP_ROOT/bin:$PATH"' >> ~/.zshrc
     echo 'export PATH="$SWIFTUP_ROOT/shims:$PATH"' >> ~/.zshrc
@@ -49,7 +49,7 @@ export PATH="$SWIFTUP_ROOT/shims:$PATH"
 
 echo $PATH
 
-if [[ -x "${HOME}/.swiftup/bin/swiftup" ]]; then
+if [[ -x "$HOME/.swiftup/bin/swiftup" ]]; then
   echo "Successfully installed swiftup"
   ~/.swiftup/bin/swiftup version
 else
