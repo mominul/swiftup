8c20a4d224420ccc8561966aa9b3ab8bea69c4f0
mominul <mominul2082@gmail.com>
2017-01-24 01:31:57 +0600

    Add Errors.swift

---
+++ Sources/SwiftupFramework/Errors.swift
@@ -0,0 +1,23 @@
+/*
+  swiftup
+  The Swift toolchain installer
+
+  Copyright (C) 2016-present swiftup Authors
+
+  Authors:
+    Muhammad Mominul Huque
+*/
+
+public enum SwiftupFramework: Error, CustomStringConvertible {
+  case installationError(description: String)
+  case internalError(description: String)
+
+  public var description: String {
+    switch self {
+    case let .installationError(description):
+      return description
+    case let .internalError(description):
+      return description
+    }
+  }
+}
